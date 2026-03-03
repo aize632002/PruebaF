ead6ca227bd01bddad61fda864df434c3f3890b9
aize632002 <velasquezalexander28@gmail.com>
2026-03-02 23:56:03 -0500

    inicio creacion y edicion de la base de datos terminado la creacion de la base de datos

---
+++ app/src/main/java/data/local/entity/AppDatabase.kt
@@ -0,0 +1,34 @@
+package data.local.entity
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import data.local.entity.Laboratorio
+import data.local.entity.Equipo
+import data.local.entity.dao.LaboratorioDao
+//import data.local.entity.dao.EquipoDao
+
+@Database(entities = [Laboratorio::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun laboratorioDao(): LaboratorioDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                // Aquí es donde te daba error. Asegúrate de que sea EXACTO:
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java, // <-- Verifica que esto no esté en rojo
+                    "techsolutions_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
--- app/src/main/java/data/local/entity/dao/EquipoDao.kt
+++ app/src/main/java/data/local/entity/dao/EquipoDao.kt
@@ -1,4 +1,28 @@
 package data.local.entity.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import data.local.entity.Equipo
+
+@Dao
 interface EquipoDao {
+    // Filtramos solo los equipos de un laboratorio específico
+    @Query("SELECT * FROM equipos WHERE laboratorioId = :labId")
+    fun obtenerEquiposPorLaboratorio(labId: Int): Flow<List<Equipo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarEquipo(equipo: Equipo)
+
+    @Update
+    suspend fun actualizarEquipo(equipo: Equipo)
+
+    @Delete
+    suspend fun eliminarEquipo(equipo: Equipo)
+
+
 }
